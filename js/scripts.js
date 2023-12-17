6b354a1a5907a580434103ad7ae08531cb23267a
word1ess <118170864+word1ess@users.noreply.github.com>
2023-12-17 14:37:01 +0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -17957,15 +17957,13 @@ var Swiper = (function () {
               height: i.outerHeight(!0),
               float: i.css("float"),
             },
-            t = V("<div></div>")
-              .addClass("ui-effects-wrapper")
-              .css({
-                fontSize: "100%",
-                background: "transparent",
-                border: "none",
-                margin: 0,
-                padding: 0,
-              }),
+            t = V("<div></div>").addClass("ui-effects-wrapper").css({
+              fontSize: "100%",
+              background: "transparent",
+              border: "none",
+              margin: 0,
+              padding: 0,
+            }),
             e = { width: i.width(), height: i.height() },
             n = document.activeElement;
           try {
@@ -19277,14 +19275,11 @@ var Swiper = (function () {
                   return 0 === parseInt(V(this).attr("tabIndex"), 10);
                 })
                 .attr("tabIndex", -1),
-          e
-            .attr("aria-hidden", "false")
-            .prev()
-            .attr({
-              "aria-selected": "true",
-              "aria-expanded": "true",
-              tabIndex: 0,
-            });
+          e.attr("aria-hidden", "false").prev().attr({
+            "aria-selected": "true",
+            "aria-expanded": "true",
+            tabIndex: 0,
+          });
       },
       _animate: function (t, i, e) {
         var s,
@@ -28380,13 +28375,11 @@ var Swiper = (function () {
       this._setOptionDisabled(this.options.disabled),
         this._setupEvents(this.options.event),
         this._setupHeightStyle(this.options.heightStyle),
-        this.tabs
-          .not(this.active)
-          .attr({
-            "aria-selected": "false",
-            "aria-expanded": "false",
-            tabIndex: -1,
-          }),
+        this.tabs.not(this.active).attr({
+          "aria-selected": "false",
+          "aria-expanded": "false",
+          tabIndex: -1,
+        }),
         this.panels
           .not(this._getPanelForTab(this.active))
           .hide()
@@ -29007,7 +29000,87 @@ var Swiper = (function () {
   V.ui.tooltip;
 });
 
-
+/*!
+ * jQuery UI Touch Punch 0.2.3
+ *
+ * Copyright 2011–2014, Dave Furfero
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ *
+ * Depends:
+ *  jquery.ui.widget.js
+ *  jquery.ui.mouse.js
+ */
+!(function (a) {
+  function f(a, b) {
+    if (!(a.originalEvent.touches.length > 1)) {
+      a.preventDefault();
+      var c = a.originalEvent.changedTouches[0],
+        d = document.createEvent("MouseEvents");
+      d.initMouseEvent(
+        b,
+        !0,
+        !0,
+        window,
+        1,
+        c.screenX,
+        c.screenY,
+        c.clientX,
+        c.clientY,
+        !1,
+        !1,
+        !1,
+        !1,
+        0,
+        null
+      ),
+        a.target.dispatchEvent(d);
+    }
+  }
+  if (((a.support.touch = "ontouchend" in document), a.support.touch)) {
+    var e,
+      b = a.ui.mouse.prototype,
+      c = b._mouseInit,
+      d = b._mouseDestroy;
+    (b._touchStart = function (a) {
+      var b = this;
+      !e &&
+        b._mouseCapture(a.originalEvent.changedTouches[0]) &&
+        ((e = !0),
+        (b._touchMoved = !1),
+        f(a, "mouseover"),
+        f(a, "mousemove"),
+        f(a, "mousedown"));
+    }),
+      (b._touchMove = function (a) {
+        e && ((this._touchMoved = !0), f(a, "mousemove"));
+      }),
+      (b._touchEnd = function (a) {
+        e &&
+          (f(a, "mouseup"),
+          f(a, "mouseout"),
+          this._touchMoved || f(a, "click"),
+          (e = !1));
+      }),
+      (b._mouseInit = function () {
+        var b = this;
+        b.element.bind({
+          touchstart: a.proxy(b, "_touchStart"),
+          touchmove: a.proxy(b, "_touchMove"),
+          touchend: a.proxy(b, "_touchEnd"),
+        }),
+          c.call(b);
+      }),
+      (b._mouseDestroy = function () {
+        var b = this;
+        b.element.unbind({
+          touchstart: a.proxy(b, "_touchStart"),
+          touchmove: a.proxy(b, "_touchMove"),
+          touchend: a.proxy(b, "_touchEnd"),
+        }),
+          d.call(b);
+      });
+  }
+})(jQuery);
 
 
 
@@ -29021,6 +29094,8 @@ if (window.screen.width < 992) {
     $(this).toggleClass("footer-active").next().slideToggle(300);
   });
 }
+
+$(".ui-slider-handle").draggable();
 
 // Удаление активных классов
 function removeActiveClass(arr, activeClass) {
